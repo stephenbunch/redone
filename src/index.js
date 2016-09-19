0b7a8a8692038c84001f5d964befa1d4a5630030
Stephen Bunch <bunchclone@gmail.com>
2016-09-18 18:49:06 -0700

    Add build.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export * as schemas from './schemas';
+
+export Computation from './Computation';
+export Dependency from './Dependency';
+export KeyedDependency from './KeyedDependency';
+export SchemaParser from './SchemaParser';
+export * as Types from './Types';
