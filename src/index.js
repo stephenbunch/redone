2ff14e1e7a9e209fc9f1b90c8fb5d583da3e1dfe
Jenny Steele <jenny@ridecell.com>
2016-09-22 10:45:12 -0700

    Export connect function in index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export Autorun from './Autorun';
 export Dependency from './Dependency';
 export KeyedDependency from './KeyedDependency';
 export SchemaParser from './SchemaParser';
+export connect from './connect';
 
 export * as schemas from './schemas';
 export * as types from './types';
