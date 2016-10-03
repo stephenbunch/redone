ab50609bb63912cd372d908c384494b15fe6a08f
Stephen Bunch <bunchclone@gmail.com>
2016-10-02 20:13:28 -0700

    Add test.

--- src/Autorun.spec.js
+++ src/Autorun.spec.js
@@ -311,6 +311,34 @@ describe('the onceAsync function', () => {
   it('should forward the return value', async () => {
     expect(await Autorun.onceAsync(() => Promise.resolve(2))).toBe(2);
   });
+
+  it('should resume on error', async () => {
+    const dep = new Dependency();
+    let called = 0;
+    const autorun = Autorun.start(() => {
+      dep.depend();
+      called += 1;
+    });
+    expect(called).toBe(1);
+
+    const error = new Error('test');
+    try {
+      await Autorun.onceAsync(async () => {
+        dep.changed();
+        dep.changed();
+        await Promise.reject(error);
+        dep.changed();
+      });
+    } catch (err) {
+      expect(err).toBe(error);
+    }
+    expect(called).toBe(2);
+
+    dep.changed();
+    expect(called).toBe(3);
+
+    autorun.dispose();
+  });
 });
 
 describe('the exclude function', () => {
